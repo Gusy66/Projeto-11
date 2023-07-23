955bf68ff4822d08ccd53c4fa345b00277d868f5
Gusy66 <62562141+Gusy66@users.noreply.github.com>
2023-07-22 21:41:35 -0300

    Update atualizar_perfil.php

--- atualizar_perfil.php
+++ atualizar_perfil.php
@@ -2,24 +2,24 @@
 
 require_once "conn.php";
 session_start();
-// Verifica se o usuário está logado
+// verifica se o usuário está logado
 if (isset($_SESSION["id"])) {
     
 
     $conexao = new mysqli($host, $usuario, $senhaBD, $bancoDeDados);
 
-    // Verifica se houve algum erro na conexão
+    // verifica se houve algum erro na conexão
     if ($conexao->connect_error) {
         die("Erro na conexão com o banco de dados: " . $conexao->connect_error);
     }
 
-    // Verifica se a foto de perfil foi enviada e atualiza no banco de dados
+    // verifica se a foto de perfil foi enviada e atualiza no banco de dados
     if (isset($_FILES["foto_perfil"]) && $_FILES["foto_perfil"]["error"] === UPLOAD_ERR_OK) {
         $pastaUploads = "uploads/"; // Diretório onde as fotos de perfil serão armazenadas
         $nomeArquivo = uniqid() . "-" . $_FILES["foto_perfil"]["name"];
         move_uploaded_file($_FILES["foto_perfil"]["tmp_name"], $pastaUploads . $nomeArquivo);
 
-        // Atualiza o campo "foto_perfil" na tabela "usuarios"
+        // atualiza o campo "foto_perfil" na tabela "usuarios"
         $idUsuario = $_SESSION["id"];
         $sql = "UPDATE usuarios SET foto_perfil = ? WHERE id = ?";
         $stmt = $conexao->prepare($sql);
@@ -28,7 +28,7 @@ if (isset($_SESSION["id"])) {
         $stmt->close();
     }
 
-    // Verifica se a biografia foi enviada e atualiza no banco de dados
+    // verifica se a biografia foi enviada e atualiza no banco de dados
     if (isset($_POST["biografia"])) {
         $biografia = $_POST["biografia"];
 
@@ -41,10 +41,10 @@ if (isset($_SESSION["id"])) {
         $stmt->close();
     }
 
-    // Redireciona de volta para a página de perfil após a atualização
+    // redireciona de volta para a página de perfil após a atualização
     header("Location: perfil.php");
 } else {
-    // Redireciona para a página de login se o usuário não estiver logado
+    // redireciona para a página de login se o usuário não estiver logado
     header("Location: login.html");
 }
 ?>
